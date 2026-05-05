4d3278cda1d689df0ab54f4e302a170e6fd1e1b2
IT25103214 <it25103214@my.sliit.lk>
2026-05-05 22:10:09 +0530

    Rental Transactions Backend

---
+++ Rental.java
@@ -0,0 +1,40 @@
+package com.movie.rental.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "rentals")
+public class Rental {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tv_series_id")
+    private TvSeries tvSeries;
+
+    @Column(nullable = false)
+    private LocalDateTime rentalDate;
+
+    private LocalDateTime returnDate;
+
+    @Enumerated(EnumType.STRING)
+    private RentalStatus status;
+}
